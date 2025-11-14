d58ab6a889c7da01808cac299248a742aa37069f
rajan-lama <rajan.lama786@gmail.com>
2025-11-14 19:49:59 +0545

    Add initial plugin files for Super AI Engine with global constants

---
+++ super-ai-engine.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name:       Super AI Engine
+ * Plugin URI:        https://github.com/rajan-lama/super-ai-engine
+ * Update URI:        https://rithemes.com
+ * Description:       Enhance your WordPress content with Artificial Intelligence and Machine Learning services.
+ * Version:           1.0.0
+ * Requires at least: 6.7
+ * Requires PHP:      7.4
+ * Author:            Rajan Lama
+ * Author URI:        https://rithemes.com
+ * License:           GPLv2
+ * License URI:       https://spdx.org/licenses/GPL-2.0-or-later.html
+ * Text Domain:       super-ai-engine
+ * Domain Path:       /languages
+ */
